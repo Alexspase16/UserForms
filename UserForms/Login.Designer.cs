b8b81bb2938f2214205e725ca25293d97cc7a26d
janakievska.vesna <janakievska.vesna@gmail.com>
2021-11-27 15:19:40 +0100

    added form for Login

---
+++ UserForms/Login.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace UserForms
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_Login = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Pass = new System.Windows.Forms.TextBox();
+            this.textBox_User = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.lblError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 109);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_Login
+            // 
+            this.button_Login.Location = new System.Drawing.Point(298, 91);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(75, 23);
+            this.button_Login.TabIndex = 9;
+            this.button_Login.Text = "Login";
+            this.button_Login.UseVisualStyleBackColor = true;
+            this.button_Login.Click += new System.EventHandler(this.button_Login_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Username";
+            // 
+            // textBox_Pass
+            // 
+            this.errorProvider1.SetError(this.textBox_Pass, "Задолжителна лозинка");
+            this.textBox_Pass.Location = new System.Drawing.Point(218, 52);
+            this.textBox_Pass.Name = "textBox_Pass";
+            this.textBox_Pass.Size = new System.Drawing.Size(155, 20);
+            this.textBox_Pass.TabIndex = 6;
+            this.textBox_Pass.UseSystemPasswordChar = true;
+            // 
+            // textBox_User
+            // 
+            this.errorProvider1.SetError(this.textBox_User, "Задолжително корисничко име");
+            this.textBox_User.Location = new System.Drawing.Point(218, 12);
+            this.textBox_User.Name = "textBox_User";
+            this.textBox_User.Size = new System.Drawing.Size(155, 20);
+            this.textBox_User.TabIndex = 5;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(135, 120);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 10;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 145);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.button_Login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Pass);
+            this.Controls.Add(this.textBox_User);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Pass;
+        private System.Windows.Forms.TextBox textBox_User;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label lblError;
+    }
+}
---
+++ UserForms/Login.cs
@@ -0,0 +1,84 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserForms
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Login_Click(object sender, EventArgs e)
+        {
+            LoginUser(textBox_User.Text, textBox_Pass.Text);
+        }
+
+        private void LoginUser(String username, String password)
+        {
+            if (username == "")
+            {
+                lblError.Text = "Username is required field!";
+                lblError.Visible = true;
+            }
+            if (password == "")
+            {
+                lblError.Text = "Password is required field!";
+                lblError.Visible = true;
+            }
+            if (username == "" && password == "")
+            {
+                lblError.Text = "Username and Password are required fields!";
+                lblError.Visible = true;
+            }
+            if (username != "" && password != "")
+            {
+
+                var connString = "Server=localhost;Port=3306;User id=root; Password=Vesna#12345;Persistsecurityinfo=True;Database=dbo";
+
+                MySqlConnection conn = new MySqlConnection(connString);
+                MySqlCommand command = conn.CreateCommand();
+
+                command.CommandText = "SELECT * FROM dbo.Users WHERE Username=@user and Password = @pass";
+
+                command.CommandType = CommandType.Text;
+
+                command.Parameters.AddWithValue("@user", username);
+                command.Parameters.AddWithValue("@pass", password);
+
+                try
+                {
+                    conn.Open();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                MySqlDataReader rdr = command.ExecuteReader();
+
+                if (rdr.Read())
+                {
+                    MessageBox.Show("Successful login!");
+                    Form1 form1 = new Form1();
+                    this.Hide();
+                    form1.ShowDialog();
+                    this.Close();
+                }
+                else
+                    MessageBox.Show("Unsuccessful login!");
+                conn.Close();
+            }
+
+        }
+    }
+}
